0e3acbd23ceeee746cf62fa2d302efcd42ebf39d
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-09-18 21:12:38 +0200

    change response headzers

--- src/Client.py
+++ src/Client.py
@@ -2,33 +2,61 @@ from flask import Response, request
 import requests
 
 
-def _get(website: str) -> Response:
+def __transform_query(
+    parameters: dict = None, headers: dict = None, cookies: dict = None
+) -> None:
+    if parameters:
+        parameters.pop("web_proxy_requested_website", None)
+    if headers:
+        headers.pop("Host", None)
+        headers["Referrer"] = __website
+    if cookies:
+        cookies.pop("web_proxy_requested_website", None)
+
+
+def __transform_response(headers: dict = None) -> None:
+    keys = ["Content-Type", "Set-Cookie"]
+    header_normalized = {key.lower(): value for key, value in headers.items()}
+    keys_normalized = [key.lower() for key in keys]
+    r_headers = {}
+    for i in range(len(keys)):
+        key = keys[i]
+        key_normalized = keys_normalized[i]
+        value = header_normalized.get(key_normalized, None)
+        if value:
+            r_headers[key] = value
+    headers.clear()
+    headers.update(r_headers)
+
+    # headers.pop("Connection", None)
+    # headers.pop("Transfer-Encoding", None)
+    # headers.pop("Content-Length", None)
+    # headers.pop("Server", None)
+    # headers.pop("Date", None)
+    # headers.pop("Strict-Transport-Security", None)
+
+
+def __get() -> Response:
     params = request.args.to_dict(True)
-    params.pop("web_proxy_requested_website", None)
     cookies = request.cookies.to_dict(True)
-    cookies.pop("web_proxy_requested_website", None)
     headers = dict(request.headers)
-    headers.pop("Host", None)
-    headers["Referrer"] = website
+    __transform_query(parameters=params, headers=headers, cookies=cookies)
     response = requests.get(
-        url=f"{website}{request.path}",
+        url=f"{__website}{request.path}",
         params=params,
         cookies=cookies,
         headers=headers,
     )
     r_headers = dict(response.headers)
-    r_headers.pop("Connection", None)
-    r_headers.pop("Transfer-Encoding", None)
-    r_headers.pop("Content-Length", None)
-    r_headers.pop("Server", None)
-    r_headers.pop("Date", None)
-    r_headers.pop("Strict-Transport-Security", None)
+    __transform_response(headers=r_headers)
     r_status = response.status_code
-    r_content = response.content
+    r_content = response.content  # check if the content is gziped or not
     return Response(response=r_content, status=r_status, headers=r_headers)
 
 
 def simple_request(website):
-    method_assoc = {"GET": _get}
+    global __website
+    __website = website
+    method_assoc = {"GET": __get}
     method_function = method_assoc[request.method]
-    return method_function(website)
+    return method_function()
