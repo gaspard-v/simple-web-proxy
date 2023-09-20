99f934d654e402ff2e9fc61d1f3623ffa1fc726e
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-09-20 12:43:09 +0200

    Add the javascript files

---
+++ javascript/webpack.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    mode: "development",
+    entry: "./src/index.js",
+    output: {
+        filename: "bundle.js",
+        path: path.resolve(__dirname, "dist"),
+        clean: true,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                },
+            },
+            {
+                test: /\.css$/i,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: "./index.html",
+        }),
+    ],
+    devtool: "eval-source-map",
+    devServer: {
+        static: "./dist",
+    },
+};
