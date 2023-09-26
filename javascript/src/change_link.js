a471b7ac6e3e6d26c348fd893042fc7a05ad066b
Gaspard Vandecasteele <gaspard.vandecasteele+fixe@gmail.com>
2023-09-26 22:57:06 +0200

    add some code

--- javascript/src/change_link.js
+++ javascript/src/change_link.js
@@ -9,19 +9,38 @@ const change_link = (link) => {
     try {
         const url = new URL(link);
         const base_url = new URL(window.location.href);
-        if (url.protocol === "data:") return link;
-        if (url.host.endsWith("xosh.fr"))
-            // TODO: change the link !!!!!!
-            // use environment variable
-            return link;
-
         const params = url.searchParams;
-        params.append("web_proxy_method", url.protocol.slice(0, -1));
-        params.append("web_proxy_port", url.port);
-        url.host = `${url.host}.${base_url.host}`;
-        url.protocol = base_url.protocol;
+        if (url.protocol === "data:") return link;
+        if (url.hostname.endsWith(process.env.WEB_PROXY_NETLOC)) {
+            const protocol = get_parameter_by_name("web_proxy_method");
+            if (protocol && !params.get("web_proxy_method"))
+                params.append("web_proxy_method", protocol);
+            const port = get_parameter_by_name("web_proxy_port");
+            if (port && !params.get("web_proxy_port"))
+                params.append("web_proxy_port", protocol);
+            return url.href;
+        }
+        const protocol = url.protocol.slice(0, -1);
+        if (!params.get("web_proxy_method")) {
+            if (protocol) params.append("web_proxy_method", protocol);
+            else {
+                const protocol = get_parameter_by_name("web_proxy_method");
+                if (protocol) params.append("web_proxy_method", protocol);
+            }
+        }
+        if (!params.get("web_proxy_port")) {
+            const port = url.port;
+            if (port) params.append("web_proxy_port", url.port);
+            else {
+                const port = get_parameter_by_name("web_proxy_port");
+                if (port) params.append("web_proxy_port", protocol);
+            }
+        }
         url.search = params;
-        return url.toString();
+        url.host = `${url.hostname}.${process.env.WEB_PROXY_NETLOC}`;
+        if (process.env.WEB_PROXY_PORT) url.host += `:${base_url.port}`;
+        url.protocol = base_url.protocol;
+        return url.href;
     } catch (error) {
         return link;
     }
